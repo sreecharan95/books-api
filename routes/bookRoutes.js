e4ccf2325e4fef45e2ce624a2484fb57f1b7d977
sreecharan95 <sreecharan289@gmail.com>
2024-05-22 20:38:30 +0530

    Added models

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bookController = require('../controllers/bookController');
 
 router.post('/', bookController.createBook);
-router.get('/', bookController.getBooks);
+router.get('/', bookController.getAllBooks);
 router.get('/:id', bookController.getBookById);
 router.put('/:id', bookController.updateBook);
 router.delete('/:id', bookController.deleteBook);
